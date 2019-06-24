91b80b4d6ff4f30a2dbcd021484ad2a3ff9e40a2
stephenpassero <stephen.passero@gmail.com>
2019-06-24 16:11:47 -0400

    Rename initialState to playerData

--- app/javascript/components/Game.js
+++ app/javascript/components/Game.js
@@ -4,17 +4,16 @@ import PlayerView from './PlayerView'
 import OpponentView from './OpponentView'
 
 export default class Game extends React.Component {
-  // Change initialState to playerData
   static propTypes = {
     id: PropTypes.number.isRequired,
     playerName: PropTypes.string.isRequired,
-    initialState: PropTypes.object.isRequired
+    playerData: PropTypes.object.isRequired
   }
 
   constructor(props) {
     super(props)
     this.state = {
-      playerData: this.props.initialState
+      playerData: this.props.playerData
     }
   }
 
@@ -33,7 +32,12 @@ export default class Game extends React.Component {
   renderOpponents() {
     return this.state.playerData.opponents.map((opponent) => { // eslint-disable-line arrow-body-style
       return (
-        <OpponentView key={opponent.name} name={opponent.name} numOfCards={opponent.cards_left} pairs={opponent.pairs} />
+        <OpponentView
+          key={opponent.name}
+          name={opponent.name}
+          numOfCards={opponent.cards_left}
+          pairs={opponent.pairs}
+        />
       )
     })
   }
--- spec/javascript/components/GameSpec.test.js
+++ spec/javascript/components/GameSpec.test.js
@@ -1,7 +1,7 @@
 import { shallow } from 'enzyme';
 import React from 'react'
 import Game from 'components/Game' // eslint-disable-line import/no-unresolved
-import initialState from './initialState'
+import playerData from './playerData'
 
 global.fetch = jest.fn(() => new Promise(resolve => resolve()))
 describe('Game', () => {
@@ -11,11 +11,11 @@ describe('Game', () => {
     wrapper = shallow(<Game
       id={3}
       playerName="Stephen"
-      initialState={initialState}
+      playerData={playerData}
     />)
   })
 
-  it('renders a one player', () => {
+  it('renders a player', () => {
     expect(wrapper.find('PlayerView').length).toEqual(1)
   })
 })
