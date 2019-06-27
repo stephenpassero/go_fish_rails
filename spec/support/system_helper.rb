812ed5f2384075e995b94754019429c2b832db38
stephenpassero <stephen.passero@gmail.com>
2019-06-27 19:27:45 -0400

    Write failing endgame test

--- spec/support/system_helper.rb
+++ spec/support/system_helper.rb
@@ -10,4 +10,12 @@ class SystemHelper
       session.click_on 'Submit'
     end
   end
+
+  def self.game_logic
+    player1 = Player.new(name: 'Player1', cards: [Card.new(rank: 'A', suit: 'Spades'), Card.new(rank: 'A', suit: 'Hearts'), Card.new(rank: 'A', suit: 'Diamonds')], pairs: [])
+    player2 = Player.new(name: 'Player2', cards: [Card.new(rank: 'A', suit: 'Clubs')], pairs: [])
+
+    players = [player1, player2]
+    game = GameLogic.new(player_names: ['Player1', 'Player2'], deck: {cards: []}, player_turn: 1, players: players, game_log: GameLog.new())
+  end
 end
